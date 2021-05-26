6792f71e219aea0da4c217d4bf33be32db4b619b
islamil95 <ihaf2012@mail.ru>
2021-05-26 23:02:23 +0300

    Делаю верхнее меню

--- test/resources/views/layouts/headerMenu.blade.php
+++ test/resources/views/layouts/headerMenu.blade.php
@@ -1,20 +1,23 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="row w-100 m-0 p-0">
-        <a class="navbar-brand p-0 col-12 m-0 col-sm-10 col-md-4" href="{{ url('/') }}">
+        <a class="navbar-brand p-0 col-12 m-0 col-sm-10 col-md-4 HeaderLogo" href="{{ url('/') }}">
 {{--            <div class="row m-0">{{ config('app.name') }}</div>--}}
             <div class="row m-0">
                 <small class="w-100">Реестр резерва педагогических</small>
                 <small class="w-100">кадров Чеченской Республики</small>
             </div>
         </a>
-        <button class="navbar-toggler col-12 col-sm-2 collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler col-12 col-sm-2 collapsed HeaderMenuButton" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
+            <ul class="navbar-nav mr-auto HeaderMenuUrlCenterBlock">
+                <li class="mx-2 HeaderMenuUrlCenter"><a class="text-muted text-decoration-none" href="{{route("home")}}">Личный кабинет</a></li>
+                <li class="mx-2 HeaderMenuUrlCenter" ><a class="text-muted text-decoration-none" href="{{route("home")}}">Документы</a></li>
+                <li class="mx-2 HeaderMenuUrlCenter"><a class="text-muted text-decoration-none" href="{{route("home")}}">Впервые на сайте?</a></li>
+                <li class="mx-2 HeaderMenuUrlCenter"><a class="text-muted text-decoration-none" href="{{route("home")}}">Контакты</a></li>
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -24,17 +24,18 @@
     @include('layouts/headerMenu')
     <div class="container   mt-4">
         <div class="row">
-            <div class="col-6"><img src="img/banner__img-1.jpg" alt="Педагогическому работнику" class="w-100 rounded"></div>
-            <div class="col-6"><img src="img/banner__img-2.jpg" alt="Руководителю образовательной организации" class="w-100 rounded">
+            <div class="col-md-6 col-12"><img src="img/banner__img-1.jpg" alt="Педагогическому работнику" class="w-100 rounded"></div>
+            <div class="col-md-6 col-12 IndexMen"><img src="img/banner__img-2.jpg" alt="Руководителю образовательной организации" class="w-100 rounded">
         </div>
         </div>
         <div class="row mt-4 d-flex justify-content-center">
-            <a class="btn btn-primary py-3 px-5" >Квалификационное испытание</a>
+            <div class="col-12 col-md-6"><a class="btn btn-primary py-3 px-5 w-100" href="{{route('login')}}">Квалификационное испытание</a></div>
         </div>
     </div>
-    <div class="row mt-4 py-5 align-items-center" style="background: #f3f3f3;">
-        <div class="col-6 w-100 h-100 d-block" style="background: url('img/platform__img.jpg');">s</div>
-        <div class="col-6">
+
+    <div class="row mx-0 mt-4  " style="background: #f3f3f3;">
+        <div class="col-12 col-md-6 IndexLeftImg" style="background: url('img/platform__img.jpg'); background-size: cover;">⠀</div>
+        <div class="col-12 col-md-6 py-5">
             <h2 class="font-weight-bold" style='font-family: "Montserrat", Arial, sans-serif;'>
                 Описание платформы
             </h2>
@@ -49,7 +50,18 @@
                 интересную работу.</p>
         </div>
     </div>
-{{--            <example-component></example-component>--}}
+<div style="background: #333333;font-size: 130%;" class="p-4 text-white row m-0">
+    <div class="col-sm-4 col-12 footer_logo">
+        <small class="w-100">Pedagogchr.ru</small>
+    </div>
+    <div class="col-sm-4 text-center col-12" >
+        <small class="w-100">© 2019 Реестр резерва педагогических кадров Чеченской Республики</small>
+    </div>
+    <div class="col-sm-4 col-12  footer_politica" >
+        <small class="w-100"><a href="/" class="text-white">Политика конфиденциальности</a></small>
+    </div>
+</div>
+    {{--            <example-component></example-component>--}}
 {{--                        <prop-component :urldata="{{json_encode($url_data)}}"></prop-component>--}}
 {{--            <ajax-component></ajax-component>--}}
 
