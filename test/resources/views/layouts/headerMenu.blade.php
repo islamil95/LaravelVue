62504a59d1ec6fffa684f16f17079422d8900d05
coko <chr_coko.pto@mail.ru>
2021-05-26 18:01:34 +0300

    Делаю главную страницу

---
+++ test/resources/views/layouts/headerMenu.blade.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="row w-100 m-0 p-0">
+        <a class="navbar-brand p-0 col-12 m-0 col-sm-10 col-md-4" href="{{ url('/') }}">
+{{--            <div class="row m-0">{{ config('app.name') }}</div>--}}
+            <div class="row m-0">
+                <small class="w-100">Реестр резерва педагогических</small>
+                <small class="w-100">кадров Чеченской Республики</small>
+            </div>
+        </a>
+        <button class="navbar-toggler col-12 col-sm-2 collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Вход
+                        </a>
+                        <div class="dropdown-menu p-2" aria-labelledby="navbarDropdownMenuLink" style="left: -120%;">
+                            @if (Route::has('login'))
+                                <a class="nav-link dropdown-item" href="{{ route('login') }}"   >Войти</a>
+                            @endif
+                            @if (Route::has('register'))
+                                <a class="nav-link dropdown-item" href="{{ route('register') }}">Регистрация</a>
+                            @endif
+                        </div>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,68 +21,40 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Войти</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+    @include('layouts/headerMenu')
+    <div class="container   mt-4">
+        <div class="row">
+            <div class="col-6"><img src="img/banner__img-1.jpg" alt="Педагогическому работнику" class="w-100 rounded"></div>
+            <div class="col-6"><img src="img/banner__img-2.jpg" alt="Руководителю образовательной организации" class="w-100 rounded">
         </div>
-    </nav>
-    <div id="app">
-            <a href="/get-json" class="btn border">Ajax проверка</a>
-            <example-component></example-component>
-                        <prop-component :urldata="{{json_encode($url_data)}}"></prop-component>
-            <ajax-component></ajax-component>
         </div>
-    <main class="py-4">
-        @yield('content')
-    </main>
+        <div class="row mt-4 d-flex justify-content-center">
+            <a class="btn btn-primary py-3 px-5" >Квалификационное испытание</a>
+        </div>
+    </div>
+    <div class="row mt-4 py-5 align-items-center" style="background: #f3f3f3;">
+        <div class="col-6 w-100 h-100 d-block" style="background: url('img/platform__img.jpg');">s</div>
+        <div class="col-6">
+            <h2 class="font-weight-bold" style='font-family: "Montserrat", Arial, sans-serif;'>
+                Описание платформы
+            </h2>
+            <p style="    text-align: justify;">Уважаемые коллеги, педагоги и руководители ОО! <br> Сайт "Реестр
+                резерва педагогических кадров Чеченской Республики" создан в целях оказания профессионального
+                сопровождения учителей и директоров школ в вопросах трудоустройства. <br> Настоящий сайт
+                ориентирован на создание региональной базы педагогических кадров ЧР, дает возможность
+                руководителям ОО подобрать для своей организации квалифицированных специалистов.</p>
+            <p style="    text-align: justify;">Уважаемые учителя! На нашем сайте вы можете пройти тестирование
+                (квалификационное испытание) по соответствующему предмету и получить сертификат о прохождении
+                данного испытания. <br>Это дает вам возможность подтвердить свою компетентность и найти
+                интересную работу.</p>
+        </div>
+    </div>
+{{--            <example-component></example-component>--}}
+{{--                        <prop-component :urldata="{{json_encode($url_data)}}"></prop-component>--}}
+{{--            <ajax-component></ajax-component>--}}
+
+
+
 </div>
 </body>
 </html>
